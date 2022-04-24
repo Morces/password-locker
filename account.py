9dc3c595120dbf2770550f78a9d874dd327e6c0f
Moses Karani <karanim594@gmail.com>
2022-04-24 12:43:17 +0300

    created a class for user instances

---
+++ account.py
@@ -0,0 +1,13 @@
+class User:
+    '''
+    Class which generates new user instances
+    '''
+    user_lists = []
+
+    # constructor for username and password
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+     
