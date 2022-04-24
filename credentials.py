8aba6617508dfd2f4a1eebab58d1c6b5df3eac8a
Moses Karani <karanim594@gmail.com>
2022-04-24 13:12:41 +0300

    added a class and function that defines object properties

---
+++ credentials.py
@@ -0,0 +1,16 @@
+class Credentials:
+
+    '''
+    This class generates new instances of credentials
+    '''
+
+    def __init__(self, account_username, account_password):
+
+        '''
+        function that defines object properties
+        '''
+
+        self.account_username = account_username
+        self.account_password = account_password
+
+    
