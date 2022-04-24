a992a335cd3d965bee9733167932265cbb13a360
Moses Karani <karanim594@gmail.com>
2022-04-24 13:40:46 +0300

    create a function that creates a new user

---
+++ run.py
@@ -0,0 +1,11 @@
+import random
+import string
+from account import User
+
+def create_user(username, password):
+    '''
+    This function creates a new user
+    '''
+    new_user = User(username, password)
+    return new_user
+
