5491ca60c525e3441a3676a1dcb990e5c8c3fc22
Moses Karani <karanim594@gmail.com>
2022-04-24 14:07:12 +0300

    optimized the test file

--- user_test.py
+++ user_test.py
@@ -1,4 +1,5 @@
-import imp
+import string
+import random
 import unittest
 from account import User
 
@@ -10,6 +11,22 @@ class Testuser(unittest.TestCase):
         unittest.TestCase: TestCase class that helps create test cases
     '''
 
-    
+    def SetUp(self):
+        self.new_account = User("Morces", "Emkay33")
+
+    def test_init(self):
+        self.assertEqual(self.new_account.username, "Morces")
+        self.assertEqual(self.new_account.password, "emkay33")
+
+    def test_save_user(self):
+        self.save_user()
+        self.assertEqual(len(User.user_list),1)
+    def test_delete_user(self):
+        self.delete()
+        self.assertEqual(len(User.user_list),0)
+
+    if __name__ == '__main__' :
+        unittest.main()
+
 
     
