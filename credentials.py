209da3bbfb1b42a97ae4865cbb137250ff22faf0
Moses Karani <karanim594@gmail.com>
2022-04-24 13:16:26 +0300

    added function for deleting user credentials

--- credentials.py
+++ credentials.py
@@ -1,8 +1,13 @@
+import random
+import string
+
+
 class Credentials:
 
     '''
     This class generates new instances of credentials
     '''
+    credentials_list = []
 
     def __init__(self, account_username, account_password):
 
@@ -13,4 +18,10 @@ class Credentials:
         self.account_username = account_username
         self.account_password = account_password
 
-    
+    def delete_credentials(self):
+
+        '''
+        This method deletes the credential object
+        '''
+
+        Credentials.credentials_list.remove(self)
