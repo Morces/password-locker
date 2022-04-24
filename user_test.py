768b94cdc8684c420addd05167e42ced738742b6
Moses Karani <karanim594@gmail.com>
2022-04-24 13:46:23 +0300

    created a test class for user behaviour

--- user_test.py
+++ user_test.py
@@ -2,3 +2,14 @@ import imp
 import unittest
 from account import User
 
+class Testuser(unittest.TestCase):
+    '''
+    This test class defines test cases for the User class behaviour
+
+    Args:
+        unittest.TestCase: TestCase class that helps create test cases
+    '''
+
+    
+
+    
